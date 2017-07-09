a9218f47e97a953d1bac133ef688c007ae15868b
Michael Allen <michaelallen315@gmail.com>
2017-07-09 19:52:05 +0200

    some styles

---
+++ front-page.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<section class="section_content">
+
+	<article>
+
+		<div class="wrapper">
+
+			<h1>Test</h1>
+
+		</div>
+
+	</article>
+
+</section>
+
+<?php get_footer();
--- page.php
+++ page.php
@@ -9,7 +9,23 @@ get_header();
 while ( have_posts() ) { the_post();
 ?>
 
-Page
+<?php include 'includes/title-wrap.php'; ?>
+
+<section class="container_wrap">
+
+	<div class="wrapper">
+
+		<div class="content_wrap">
+
+			<div class="page_content">
+				<?= the_content() ?>
+			</div>
+
+		</div>
+
+	</div>
+
+</section>
 
 <?php
 }
